d15dc2183d03fd5f4a3e511924b211a2ffd90a7f
mahede <mahede.object@gmail.com>
2021-11-17 08:25:06 -1200

    Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import Hero from '../component/Hero'
+import Banner from '../component/Bannner'
+import { Link } from 'react-router-dom'
 const Home = () => {
     return (
-        <div>
-            this is home
-        </div>
+        <Hero>
+            <Banner
+             title="luxurios rooms"
+             subtitle="deluxe rooms starting at $299">
+                 <Link to='/rooms' className="btn-primary">
+                     our rooms
+                 </Link>
+            </Banner>
+        </Hero>
     )
 }
 export default Home
